bcbf36af06566b7d4514c2354e1364ca7ddcfb0b
карина сакович <102471349+7898h@users.noreply.github.com>
2022-03-30 11:00:07 +0000

    added factorial search

--- .ccls-cache/@home@runner@OnerlookedCornsilkTranslations/main.cpp
+++ .ccls-cache/@home@runner@OnerlookedCornsilkTranslations/main.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
-
-int main() {
-  std::cout << "Hello World!\n";
-}
+#include <iostream> 
+using namespace std; 
+int main() 
+{ setlocale(0,".1251"); 
+  int a[17],  max, f=1, i,j;  
+cout<<"\nВведiть 17 цілих елементів масиву:\n"; 
+for (int i=0; i<17; i++)  cin >> a[i]; 
+max = a[0];
+for (i=0; i<17; i++)
+if (a[i] > max) 
+{ max=a[i];}
+cout<<"\nМаксимальне значення = " << max <<endl;
+for (j=1; j<=max;j++ ) {f*=j;}
+cout<<"Факторіал максимального значення = " << f <<endl;
+  system ("pause>>void"); 
+  return 0; 
+} 
